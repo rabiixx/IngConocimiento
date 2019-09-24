97991b60d6922eb9e9197219d0542a53500515c7
rabiixx <rabiixx@gmail.com>
2019-09-24 19:09:26 +0200

    added pratica1a ejer1

---
+++ Practicas/Practica1a/practica1a.m
@@ -0,0 +1,45 @@
+%{
+X = 1:10;
+
+mu1_x = [0 0 0.2 0.5 0.7 1 0.7 0.5 0.2 0];
+
+figure;
+plot(X, mu1_x, 'om');
+title('Funciones de pertenencia');
+hold on;
+
+mu2_x = [1 1 0.7 0.5 0.2 0 0 0 0 0];
+plot(X, mu2_x, 'ob');
+
+legend('Conjunto 1','Conjunto 2'); 
+%}
+
+X = 1:7;
+mu1_a = [0.1 0.3 0.7 1.0 0.6 0.2 0.1];
+mu2_b = [0.2 0.8 1.0 0.6 0.4 0.3 0.1];
+
+figure;
+plot(X, mu1_a, 'om-');
+title('Funciones de pertenencia');
+hold on;
+plot(X, mu2_b, 'ob-');
+
+%% A U B
+U = min(min(mu1_a, [], 7), min(mu2_b, [], 7));
+plot(X, U, 'ok-'); 
+%% A ∩ B
+I = max(max(mu1_a, [], 7), min(mu2_b, [], 7));
+plot(X, I, 'og-'); 
+
+legend('A','B', 'min(A, B)','max(A,B)');
+
+%% C(B)
+C = ones(1, 7) 
+
+C = ones(1, 7)  - mu2_b;
+
+%% C(A U B)
+D =  ones(1, 7) - min(min(mu1_a, [], 7), min(mu2_b, [], 7));
+ 
+%% C(C(A) ∩ B) 
+E = ones(1, 7) - max(max((ones(1,7) - mu1_a), [], 7), max(mu2_b, [], 7));
